1a814ee478b9ae7f2c1fc60684103e2238629698
Weijing Wang <weijingrocks@gmail.com>
2022-09-07 00:57:46 -0700

    alll ingredients with counters

--- main.py
+++ main.py
@@ -10,15 +10,6 @@ pot = pygame.image.load("./assets/pot.png").convert_alpha()
 pot = pygame.transform.scale(pot,(200,200))
 pot_rect = pot.get_rect()
 
-
-redbeans = pygame.image.load("./assets/red_beans.png").convert_alpha()
-redbeans = pygame.transform.scale(redbeans,(200,200))
-redbeans_rect = redbeans.get_rect()
-
-sugar = pygame.image.load("./assets/sugar.png").convert_alpha()
-sugar = pygame.transform.scale(sugar,(200,200))
-sugar_rect = sugar.get_rect()
-
 customer = pygame.image.load("./assets/customer_test.png").convert_alpha()
 customer = pygame.transform.scale(customer,(500,500))
 customer_rect = customer.get_rect()
@@ -41,6 +32,7 @@ class KitchenThings(pygame.sprite.Sprite):
 
 		self.rect =self.image.get_rect()
 		self.rect.center = self.coords
+		self.coords_og = self.coords
 		self.kind = kind
 
 		self.can_activate = True
@@ -51,8 +43,16 @@ class KitchenThings(pygame.sprite.Sprite):
 		if active==True and self.rect.collidepoint(mouse_pos):
 			self.display.blit(self.image_big,self.rect)
 			if self.can_activate==True:
+				# if self.rect.colliderect(pygame.Rect(200,200,350,550))==False:
+				# 	print
+				# 	self.rect.move_ip((350-self.rect.center[0])/4, (550-self.rect.center[1])/4)
+				# else:
 				self.can_activate = False
-				print(self.kind)
+
+				# print(self.kind)
+				# print(self.rect.center)
+				return self.kind
+
 				
 		else:
 			self.can_activate=True
@@ -65,6 +65,11 @@ class KitchenThings(pygame.sprite.Sprite):
 RedBeans = KitchenThings("./assets/red_beans.png",(150,150),screen,'beans',200,200)
 Sugar = KitchenThings("./assets/sugar.png",(150,400),screen,'sugar',200,200)
 Water = KitchenThings("./assets/water.png",(820,590),screen,'water',325,195)
+Heat = KitchenThings("./assets/heat.png",(1170,600),screen,'heat',213,114)
+
+
+order = [0,0,0,0]
+#beans,sugar,water,heat
 
 while not done:
 	for event in pygame.event.get():
@@ -78,10 +83,21 @@ while not done:
 
 	screen.blit(draft,(0,0))
 	screen.blit(pot,(300,500))
-	RedBeans.Draw(mouse_press,mouse_pos)
-	Sugar.Draw(mouse_press,mouse_pos)
-	Water.Draw(mouse_press,mouse_pos)
+	bean_counter = RedBeans.Draw(mouse_press,mouse_pos)
+	sugar_counter = Sugar.Draw(mouse_press,mouse_pos)
+	water_counter = Water.Draw(mouse_press,mouse_pos)
+	heat_counter = Heat.Draw(mouse_press,mouse_pos)
 
+	if bean_counter =='beans':
+		order[0]+=1
+	elif sugar_counter =='sugar':
+		order[1]+=1
+	elif water_counter =='water':
+		order[2]+=1
+	elif heat_counter =='heat':
+		order[3]+=1
+
+	print(order)
 	screen.blit(customer,(570,20))
 
 
