5a43696001089e5bc4146937ba79e66e28371317
Weijing Wang <weijingrocks@gmail.com>
2022-09-06 21:32:42 -0700

    placing ingredients

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ screen = pygame.display.set_mode((1280, 720))
 draft = pygame.image.load("./assets/draft.png").convert_alpha()
 
 
-class Ingredients():
+class Ingredients(pygame.sprite.Sprite):
 	"""docstring for ClassName"""
 	def __init__(self, image_link, coords, display):
+		super().__init__()
 		self.image_link = image_link
 		self.image = pygame.image.load(self.image_link).convert_alpha()
 		self.rect = self.image.get_rect()
@@ -16,34 +17,51 @@ class Ingredients():
 	def Render(self):
 		self.display.blit(pygame.transform.scale(self.image,(200,200)),(self.coords[0],self.coords[1]))
 
-class Pot():
+class Pot(pygame.sprite.Sprite):
 	"""subclass of ingredients?"""
 	def __init__(self, coords, display):
+		super().__init__()
 		self.display = display
 		self.image = pygame.image.load('./assets/pot.png').convert_alpha()
 		self.image = pygame.transform.scale(self.image, (200, 200))
 		self.rect =self.image.get_rect()
+		self.move_object = False
+		self.coord_store = (400,630)
+		self.click_activate_count =0
 
 	def Render(self,coords):
 
-		self.rect = (coords[0]-100,coords[1]-100)
-		# if x<=315:
-		# 	x = 315
-		# elif x >= 1280:
-		# 	x = 1280
-		# elif y <= 0+200:
-		# 	y = 0
-		# elif y >= 720:
-		# 	y = 720
+		# if self.rect.collidepoint(coords) ==True:
+		# 	print('poo')
 
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				self.click_activate_count +=1
+
+				if self.click_activate_count ==2:
+					self.click_activate_count=0
+					self.coord_store = coords
+					self.move_object = False
+
+			elif event.type == pygame.MOUSEBUTTONUP and self.click_activate_count==1:
+				self.move_object = True
+
+		if self.move_object == True:
+			self.rect = (coords[0]-100,coords[1]-100)
+		else:
+			self.rect = (self.coord_store[0]-100,self.coord_store[1]-100)
 
 		self.display.blit(self.image,self.rect)
 
 
 
-class Customer():
+class Customer(pygame.sprite.Sprite):
 	"""docstring for ClassName"""
 	def __init__(self, display):
+		super().__init__()
 		self.display = display
 		self.image = pygame.image.load('./assets/customer_test.png').convert_alpha()
 		self.rect = self.image.get_rect()
@@ -61,11 +79,8 @@ Customers = Customer(screen)
 
 while not done:
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			quit()
 	mouse_pos = pygame.mouse.get_pos()
-	print(mouse_pos)
+	# print(mouse_pos)
 	screen.blit(draft,(0,0))
 	MyPot.Render(mouse_pos)
 
